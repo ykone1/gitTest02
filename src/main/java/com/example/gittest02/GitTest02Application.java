d736544bbc61f9b21d6ee534275e601a5ce36ea9
yukemeng <2869432967@qq.com>
2022-08-09 10:57:02 +0800

    initCommit

---
+++ src/main/java/com/example/gittest02/GitTest02Application.java
@@ -0,0 +1,13 @@
+package com.example.gittest02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTest02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitTest02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gittest02/GitTest02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gittest02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTest02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
